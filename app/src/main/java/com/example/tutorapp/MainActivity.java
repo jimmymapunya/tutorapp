432d1d2abde8d33ff0e8e27e68f7b327a4e19db5
Maribolle Rakolota <Maribolle.Rakolota@mixtelematics.com>
2019-05-01 16:41:06 +0200

    Removed FAB and updated the layout for the main screen.

--- app/src/main/java/com/example/tutorapp/MainActivity.java
+++ app/src/main/java/com/example/tutorapp/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.tutorapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,14 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "I am Riolas The Boss", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
